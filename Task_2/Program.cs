036d9ea1aed57e12f80dee8af4b9b5e2ef8ac228
AlinaTaifurova <120791016+AlinaTaifurova@users.noreply.github.com>
2023-02-05 18:48:06 +0300

    Add files via upload

---
+++ Task_2/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Задача 2: 
+Задайте одномерный массив, заполненный случайными числами.
+Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 26
+[-4, -6, 4, 6] -> 0*/
+
+Console.Write("Введи размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] myArray = new int[size];
+{
+    Random rnd = new Random();
+    for (int x = 0; x < myArray.Length; x++)
+    {
+        myArray[x] = rnd.Next(100, 1000);
+        Console.WriteLine("Значение элемента массива " + x + " = " + myArray[x]); //Могу сделать просто в ряд, если нужно//
+    }
+}
+int SumOdd(int[] myArray)
+{
+    int sum = 0;
+    for (int x = 1; x < myArray.Length; x += 2)
+    {
+        {
+            sum = sum + myArray[x];
+        }
+    }
+    return sum;
+}
+System.Console.WriteLine($"Сумма элементов стоящих на нечетных позициях: {SumOdd(myArray)}");
---
+++ Task_3/Program.cs
@@ -0,0 +1,61 @@
+﻿/*Задача 3: 
+Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива.
+[3, 7.4, 22.3, 2, 78] -> 76*/
+
+int InputInt(string message)
+{
+    System.Console.Write($"{message}");
+    return int.Parse(Console.ReadLine()!);
+}
+double[] myArray(int size)
+{
+    double[] array = new double[size];
+    for (int x = 0; x < array.Length; x++)
+    {
+        Random rnd = new Random();
+        double numbers = (int)Math.Round(rnd.NextDouble() * (100 - 1) + 1, 2);
+        array[x] = numbers;
+    }
+    return array;
+}
+void PrintArray(double[] array)
+{
+    foreach (var item in array)
+    {
+        System.Console.WriteLine($"{item}\t ");
+    }
+    System.Console.WriteLine();
+}
+
+double MaxNum(double[] myArray)
+{
+    double max = myArray[0];
+    for (int x = 0; x < myArray.Length; x++)
+    {
+        if (myArray[x] > max)
+        {
+            max = myArray[x];
+        }
+    }
+    return max;
+}
+double MinNum(double[] myArray)
+{
+    double min = myArray[0];
+    for (int x = 0; x < myArray.Length; x++)
+    {
+        if (myArray[x] < min)
+        {
+            min = myArray[x];
+        }
+    }
+    return min;
+}
+
+int size = InputInt("Введите длинну массива: ");
+double[] array = myArray(size);
+PrintArray(array);
+double max = MaxNum(array);
+double min = MinNum(array);
+System.Console.WriteLine($"Разница между максимальным и минимальным элементами массива: {Math.Round(max - min, 2)}");
