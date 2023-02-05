c4cd808e662f68413cffb0626d1e6d2f668c865b
AlinaTaifurova <120791016+AlinaTaifurova@users.noreply.github.com>
2023-02-05 18:43:51 +0300

    Add files via upload

---
+++ Task_1/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Задача 1: 
+Задайте массив заполненный случайными положительными трёхзначными числами. 
+Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2*/
+
+Console.Write("Введи размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] myArray = new int[size];
+{
+    Random rnd = new Random();
+    for (int x = 0; x < myArray.Length; x++)
+    {
+        myArray[x] = rnd.Next(100, 1000);
+        Console.WriteLine("Значение элемента массива " + x + " = " + myArray[x]); //Могу сделать просто в ряд, если нужно//
+    }
+}
+int OddNumbers(int[] myArray)
+{
+    int count = 0;
+    for (int x = 0; x < myArray.Length; x++)
+    {
+        if (myArray[x] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+System.Console.WriteLine($"Всего {OddNumbers(myArray)} четных числа/чисел в массиве");
